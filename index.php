cad63fdbffca3cd2002a6d46f186b01c7f393a30
wojcikf <wojcikkfilip@gmail.com>
2021-10-05 13:41:39 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,291 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="styles/bootstrap.min.css" rel="stylesheet" />
+    <link href="styles/style.css" rel="stylesheet" />
+    <link href="styles/responsive.css" rel="stylesheet" />
+    <link rel="icon" type="image/png" href="images/favicon.png" />
+    <title>BIZNESPORT.PL</title>
+  </head>
+  <body>
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-light"><a class="navbar-brand d-lg-none logo" href="#"><img src="images/logo.png" /></a><button class="navbar-toggler ms-auto" data-bs-toggle="collapse" data-bs-target="#myNavbarToggler7"><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="myNavbarToggler7">
+          <ul class="navbar-nav mx-auto align-middle">
+            <li class="nav-item"><a class="nav-link" href="#portfolio">Portfolio</a></li>
+            <li class="nav-item"><a class="nav-link" href="#about">About</a></li><a class="d-none d-lg-block logo" href="#"><img src="images/logo.png" /></a>
+            <li class="nav-item"><a class="nav-link" href="#">Blog</a></li>
+            <li class="nav-item"><a class="nav-link" href="#getInTouch">Get in touch</a></li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+    <section id="portfolio">
+      <div class="hex_row_odd">
+        <div class="center">
+          <div class="hexagon">
+            <div class="hex1">
+              <div class="hex2" style="background: url('images/portfolio/1.png') center no-repeat">
+                <div class="desc">
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="center">
+          <div class="hexagon">
+            <div class="hex1">
+              <div class="hex2" style="background: url('images/portfolio/2.png') center no-repeat">
+                <div class="desc">
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="center">
+          <div class="hexagon">
+            <div class="hex1">
+              <div class="hex2" style="background: url('images/portfolio/3.png') center no-repeat">
+                <div class="desc">
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="center">
+          <div class="hexagon">
+            <div class="hex1">
+              <div class="hex2" style="background: url('images/portfolio/4.png') center no-repeat">
+                <div class="desc">
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="hex_row_even">
+        <div class="center">
+          <div class="hexagon">
+            <div class="hex1">
+              <div class="hex2" style="background: url('images/portfolio/5.png') center no-repeat">
+                <div class="desc">
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="center">
+          <div class="hexagon">
+            <div class="hex1">
+              <div class="hex2" style="background: url('images/portfolio/6.png') center no-repeat">
+                <div class="desc">
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="center">
+          <div class="hexagon">
+            <div class="hex1">
+              <div class="hex2" style="background: url('images/portfolio/7.png') center no-repeat">
+                <div class="desc">
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="center">
+          <div class="hexagon">
+            <div class="hex1">
+              <div class="hex2" style="background: url('images/portfolio/8.png') center no-repeat">
+                <div class="desc">
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="center">
+          <div class="hexagon">
+            <div class="hex1">
+              <div class="hex2" style="background: url('images/portfolio/9.png') center no-repeat">
+                <div class="desc">
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="hex_row_odd" style="margin:auto;">
+        <div class="center">
+          <div class="hexagon">
+            <div class="hex1">
+              <div class="hex2" style="background: url('images/portfolio/10.png') center no-repeat">
+                <div class="desc">
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="center">
+          <div class="hexagon">
+            <div class="hex1">
+              <div class="hex2" style="background: url('images/portfolio/11.png') center no-repeat">
+                <div class="desc">
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="center">
+          <div class="hexagon">
+            <div class="hex1">
+              <div class="hex2" style="background: url('images/portfolio/12.png') center no-repeat">
+                <div class="desc">
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="center">
+          <div class="hexagon">
+            <div class="hex1">
+              <div class="hex2" style="background: url('images/portfolio/13.png') center no-repeat">
+                <div class="desc">
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="bullets text-start">
+        <a href=""></a>
+        <a href=""></a>
+        <a href="" class="active-bullet"></a>
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+      </div>
+    </section>
+    <section id="about">
+      <div class="container-fluid">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-3">
+              <img src="images/about-me.png" />
+            </div>
+            <div class="col-md-9">
+              <div class="text-start">
+                <h2>Smith Anderson</h2>
+                <span>Illustrator & Character Designer</span>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris porttitor, nibh at sagittis tristique, enim magna pretium augue, eget consectetur sem dui non urna. Integer gravida, diam congue molestie fringilla, elit odio rhoncus turpis, ac dictum libero elit vel metus. Morbi egestas sit amet velit eget venenatis. Mauris porttitor, nibh at sagittis tristique, enim magna pretium augue, eget consectetur sem dui non urna</p>
+                <div class="text-end">
+                  <div class="social-icons">
+                    <div class="social-icons-image d-inline">
+                      <a href="https://www.facebook.com">
+                        <img src="images/social-media/facebook.png" alt="Facebook">
+                      </a>
+                    </div>
+                    <div class="social-icons-image d-inline">
+                      <a href="https://www.twitter.com">
+                        <img src="images/social-media/twitter.png" alt="Twitter">
+                      </a>
+                    </div>
+                    <div class="social-icons-image d-inline">
+                      <a href="https://www.google.com">
+                        <img src="images/social-media/google.png" alt="Google">
+                      </a>
+                    </div>
+                    <div class="social-icons-image d-inline">
+                      <a href="https://pinterest.com">
+                        <img src="images/social-media/pinterest.png" alt="Pinterest">
+                      </a>
+                    </div>
+                    <div class="social-icons-image d-inline">
+                      <a href="https://www.linkedin.com">
+                        <img src="images/social-media/linkedin.png" alt="Linkedin">
+                      </a>
+                    </div>
+                    <div class="social-icons-image d-inline">
+                      <a href="https://www.tumblr.com">
+                        <img src="images/social-media/tumblr.png" alt="Tumblr">
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id="getInTouch">
+      <div class="container-fluid">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12 text-start heading">
+              <h2>Get In Touch</h2>
+              <span>In order to get in touch use the contact form below:</span>
+            </div>
+            <div class="col-md-9 text-start">
+              <form action="index.php">
+                <input type="text" id="fname" name="firstname" placeholder="Name (Required)" required>
+                <input type="email" id="femail" name="email" placeholder="Email (Required)" required>
+                <input type="text" id="fsubject" name="subject" placeholder="Subject (Required)" maxlength="60" required>
+                <textarea id="message" name="message" placeholder="Write your message here... (Required)" style="height:200px" required></textarea>
+                <p class="text-end">
+                  <input type="submit" value="Submit" class="text-end">
+                </p>
+              </form>
+            </div>
+            <div class="col-md-3 text-start">
+              <p class="text-justify">
+                <strong>Quisque</strong> Hendrerit: purus dapibus, ornare nibh vitae, viverra nibh. Fusce vitae aliquam tellus. Proin sit amet volutpat libero. Nulla sed nunc et tortor luctus faucibus morbi vitae.
+              </p>
+              <ul id="footer-address">
+                <li id="map-icon">
+                  <img src="images/footer/map.png" />
+                  <span>Elm St. 14/05 Lost City</span>
+                </li>
+                <li id="phone-icon">
+                  <img src="images/footer/phone.png" />
+                  <a href="tel:+ 3528 331 86 35 ">+ 3528 331 86 35</a>
+                </li>
+                <li id="envelope-icon">
+                  <img src="images/footer/envelope.png" />
+                  <a href="mailto:info@hexalcorp.com">info@hexalcorp.com</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <footer>
+      <div class="container-fluid">
+        <div class="container">
+          <div class="row">
+            <p>© Copyright 2013 by PSDchat</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <script src="scripts/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+    <script src="scripts/bootstrap.min.js"></script>
+  </body>
+</html>
